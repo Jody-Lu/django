739ed58953dc1060bdd9220834440e6b599c88d9
Jody-Lu <b00901192@ntu.edu.tw>
2016-02-26 10:06:59 +0800

    add hello world view function to get current time.

--- myweb/trips/views.py
+++ myweb/trips/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 def hello_world(request):
-	return HttpResponse("Hello World!")
+	output = """
+		<!DOCTYPE html>
+		<html>
+			<head>
+			</head>
+			<body>
+				Hello World! <em style="color:LightSeaGreen;">{current_time}</em>
+			</body>
+		</html>
+		""".format(current_time=datetime.now())
+
+	return HttpResponse(output)
+
 
